1f750a3bf3426816d2d68119764a042732cbf1fe
Toldroyd91 <thomas.oldroyd@hotmail.com>
2026-06-02 14:20:34 +0100

    Create designers.js

---
+++ designers.js
@@ -0,0 +1,30 @@
+// ==========================================
+// CENTRAL DESIGNER & BRAND CONFIGURATION
+// ==========================================
+window.designerProfiles = {
+    "Tom": { 
+        phone: "07700 900000", 
+        email: "tom@cohi.co.uk", 
+        defaultBrand: "Yorkshire Windows" 
+    },
+    "Sobaan": { 
+        phone: "07700 900001", 
+        email: "sobaan@cohi.co.uk", 
+        defaultBrand: "CO Home Improvements" 
+    },
+    "James": { 
+        phone: "07700 900002", 
+        email: "james@cohi.co.uk", 
+        defaultBrand: "CO Home Improvements" 
+    }
+};
+
+window.brandLogos = {
+    "Clearview": "clearview.png", 
+    "CO Home Improvements": "logo.jpg", 
+    "Orion Windows": "orion.png",
+    "Planet": "planet.png", 
+    "Trent Valley Windows": "trentvalley.png", 
+    "West Yorkshire Windows": "westyorkshire.png",
+    "Yorkshire Windows": "yorkshire.png"
+};
